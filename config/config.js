0d6f3953e4639303ca24a65c81a2922de392f174
GreenCord <steve@nevizen.com>
2018-02-11 10:36:53 -0500

    Config.js fix

--- config/config.js
+++ config/config.js
@@ -16,6 +16,11 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
+    username: process.env.JDBUSER,
+    password: process.env.JDBPW,
+    database: process.env.JDBDB,
+    host: process.env.JDBHOST,
+    dialect: 'mysql',
     use_env_variable: process.env.JAWSDB_URL
   }
 };
